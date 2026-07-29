89d0af4bf9ad0d25e4bbeb052c2dab5b8ea82335
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2026-07-29 09:07:05 +0530

    update

--- client/src/utils/ProjectsDetails.jsx
+++ client/src/utils/ProjectsDetails.jsx
@@ -1,23 +1,18 @@
 import ECommarce from '../assets/images/E-commarce.jpeg'
-import ToDoApp from '../assets/images/ToDoApp.jpeg'
-import ChatApp from '../assets/images/ChatApp.jpeg'
+import ToDo from '../assets/images/TODO.png'
+import Blog from '../assets/images/Blog.png'
 export const projects = [
+    
     {
-      image: ECommarce,
-      title: "Ecommerce project",
-      tech: ["React JS", "Express JS", "Node JS", "MongoDB"],
-      link: "https://github.com/yourusername/ecommerce-project",
-    },
-    {
-      image: ToDoApp,
+      image: ToDo,
       title: "To Do App",
       tech: ["React JS", "Express JS", "Node JS", "MongoDB"],
       link: "https://to-do-app-git-main-debjit-ghoshs-projects-20257a83.vercel.app",
     },
     {
-      image: ChatApp,
+      image: Blog,
       title: "Chat App",
-      tech: ["React JS", "Express JS", "Node JS", "MongoDB", "Socket.IO"],
-      link: "https://github.com/yourusername/chat-app",
+      tech: ["React JS", "Express JS", "Node JS", "MongoDB",],
+      link: "https://blog-app-xsfm.vercel.app/",
     },
   ];
