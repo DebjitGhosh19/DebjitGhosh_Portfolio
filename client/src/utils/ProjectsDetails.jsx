2530288d18b5ae3d4dbf13e32a2ee0b21c0a9aca
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2026-09-08 20:36:05 +0530

    ..

--- client/src/utils/ProjectsDetails.jsx
+++ client/src/utils/ProjectsDetails.jsx
@@ -1,18 +1,25 @@
 import ECommarce from '../assets/images/E-commarce.jpeg'
-import ToDo from '../assets/images/TODO.png'
-import Blog from '../assets/images/Blog.png'
+// import ToDo from '../assets/images/TODO.png'
+// import Blog from '../assets/images/Blog.png'
 export const projects = [
     
+    // {
+    //   image: ToDo,
+    //   title: "To Do App",
+    //   tech: ["React JS", "Express JS", "Node JS", "MongoDB"],
+    //   link: "https://to-do-app-git-main-debjit-ghoshs-projects-20257a83.vercel.app",
+    // },
+    // {
+    //   image: Blog,
+    //   title: "Chat App",
+    //   tech: ["React JS", "Express JS", "Node JS", "MongoDB",],
+    //   link: "https://blog-app-xsfm.vercel.app/",
+    // },
     {
-      image: ToDo,
-      title: "To Do App",
-      tech: ["React JS", "Express JS", "Node JS", "MongoDB"],
-      link: "https://to-do-app-git-main-debjit-ghoshs-projects-20257a83.vercel.app",
+       image: ECommarce,
+       title: "E-Commarce",
+       tech: ["React JS", "Express JS", "Node JS", "MongoDB",],
+       link: "https://ecom-two-cyan-25.vercel.app/",
     },
-    {
-      image: Blog,
-      title: "Chat App",
-      tech: ["React JS", "Express JS", "Node JS", "MongoDB",],
-      link: "https://blog-app-xsfm.vercel.app/",
-    },
+
   ];
